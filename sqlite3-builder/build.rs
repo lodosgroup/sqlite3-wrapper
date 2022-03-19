17b79480fdb0e67e7109e89ba2573d96afd10930
Onur Özkan <work@onurozkan.dev>
2022-03-19 17:38:25 +0300

    print readable error messages
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- sqlite3-builder/build.rs
+++ sqlite3-builder/build.rs
@@ -1,22 +1,30 @@
-use std::{env::var, path::Path, process::Command};
+use std::{path::Path, process::Command};
 
 fn main() {
-    let output_dir = var("OUT_DIR").unwrap();
-    let output_dir = Path::new(&output_dir);
+    let output_dir = Path::new("../target");
+    let compiled_output_name = "sqlite3_sys";
 
     let lib_status = Command::new("ld")
         .arg("-lsqlite3_sys")
         .arg("-o")
         .arg(output_dir.clone().join("tmp.o"))
         .output()
-        .unwrap()
+        .expect(
+            &format!(
+                "Got an error while executing `ld -lsqlite3_sys -o {}`",
+                output_dir.clone().join("tmp.o").display()
+            )
+            .to_owned(),
+        )
         .status;
 
     if lib_status.success() {
+        println!(
+            "cargo:warning={}",
+            "libsqlte3_sys already exists on system. Process will safely exit."
+        );
         std::process::exit(0);
     }
-
-    let compiled_output_name = "sqlite3_sys";
 
     Command::new("cc")
         .arg("-fpic")
@@ -32,21 +40,27 @@ fn main() {
                 .join(compiled_output_name.to_owned() + ".o"),
         )
         .output()
-        .expect("error-message1");
+        .expect("Couldn't compile c_source into object file.");
 
+    let dylib_path = output_dir
+        .clone()
+        .join("lib".to_owned() + compiled_output_name + ".so");
+    let ofile_path = output_dir
+        .clone()
+        .join(compiled_output_name.to_owned() + ".o");
     Command::new("cc")
         .arg("-shared")
-        .arg(
-            output_dir
-                .clone()
-                .join(compiled_output_name.to_owned() + ".o"),
-        )
+        .arg(&ofile_path)
         .arg("-o")
-        .arg(
-            output_dir
-                .clone()
-                .join("lib".to_owned() + compiled_output_name + ".so"),
-        )
+        .arg(&dylib_path)
         .output()
-        .expect("error-message2");
+        .expect(&format!(
+            "Couldn't create shared object from {}",
+            ofile_path.display()
+        ));
+
+    // set library permission as read-only
+    let mut lib_permissions = std::fs::metadata(&dylib_path).unwrap().permissions();
+    lib_permissions.set_readonly(true);
+    std::fs::set_permissions(&dylib_path, lib_permissions).expect("TODO");
 }
